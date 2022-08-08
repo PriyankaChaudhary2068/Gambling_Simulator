bc8bf1f33484cf7b40d99027d65afd51fb55e5ef
chutki12 <priyankachaudhary2068>
2022-08-08 23:45:45 +0530

    ADDED: This Code For To Know The Days Won And Lost In Each Month

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -5,15 +5,18 @@ public class GamblingSimulator {
 	
 	public static final double INITIAL_STAKE = 100;
 	public static final double STAKE_BET = 1;
-	public static final int NUMBER_OF_PLAYS =20;
-	public static int stake = 0;
+	public static final int NUMBER_OF_PLAYS =30;
+	public static int stake = 0, monthlyWins=0, monthlyLosses =0;
 	
 
 	public static boolean winOrLose() {
 
 		if ((int)Math.floor(Math.random()*10)%2 == 1) {
+			monthlyWins++;
 			return true;
+			
 		} else {
+			monthlyLosses++;
 			return false;
 		}
 	}
@@ -37,6 +40,8 @@ public class GamblingSimulator {
 
 	public static void main(String[] args) {
 		System.out.println(" Welcome To The Gambling Simulation Problem");
+	    System.out.println("...................");
+
 		double balance = 0;
 	
 		for(int numberOfDays = 0; numberOfDays<= NUMBER_OF_PLAYS ; numberOfDays++) {
@@ -44,7 +49,15 @@ public class GamblingSimulator {
 			gamblerDecision();
 			balance = balance+stake; 			
 		}
+			
 			System.out.println("Total amount : " + balance);
-		}
+		
+			double amountLostOrWon = (INITIAL_STAKE*NUMBER_OF_PLAYS) - balance;
+			System.out.println("Amount Won(-ve) or Lost(+ve) = "+amountLostOrWon);
+			System.out.println("Number of Days Lost : "+monthlyLosses);
+			System.out.println("Number of Days Won : "+monthlyWins);
+			
+	}
 
-}	
+
+}
