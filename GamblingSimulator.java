d68d3669531b67bfdd69f9ea33b36ec5c13aa4a2
chutki12 <priyankachaudhary2068>
2022-08-08 23:23:12 +0530

    ADDED: This Code For To Gambler Calculate If Won Or Lost

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -3,38 +3,42 @@ package com.bridgelabz.gamblingSimulator;
 
 public class GamblingSimulator {
 	
-	
-	static int INITIAL_STAKE = 100;
-    static int STAKE_BET = 1;
-    public static int stake = 0;
+	 public static void main(String[] args) {
+		 
+	        System.out.println(" Welcome To The Gambling Simulation Problem");
+	        int total_Amount = 100;
+	        int bet_Amount = 1;
+	        while (total_Amount > 50 && total_Amount < 150) {
+	            int gameAmount = (int) Math.floor(Math.random() * 10) % 2;
 
-	public static void winOrLose() {
+	            System.out.println("Gambler received amount : " + gameAmount);
 
-		if (Math.random() < 0.5) {
-			INITIAL_STAKE++;
-			System.out.println("Player Win");
-			
-		} 
-		else {
-			INITIAL_STAKE--;
-			System.out.println("Player Loose");
+	            if (bet_Amount == gameAmount) {
+	                System.out.println("Gambler won the current game");
+	                total_Amount = total_Amount + bet_Amount;
+	                System.out.println("Total Current case of gambler has : " + total_Amount);
 
-		}
+	            } else {
+	                System.out.println("Gambler lost the  current game");
+	                total_Amount = total_Amount - bet_Amount;
+	                System.out.println("Total Current case of gambler has : " + total_Amount);
+
+	            }
+	        }
+	        System.out.println("Total amount received by gambler has :" + total_Amount);
+
+	        if (total_Amount == 50 || total_Amount == 150) {
+	            System.out.println(".....................");
+	            System.out.println("Player would like to resign for current day !");
+	            System.out.println("....................");
+	        }
+
+
+	    }
+
+
 	}
 
 	
-    public static void main(String[] args) {
-		
-		 System.out.println(" Welcome To The Gambling Simulation Problem");
-		 System.out.println("...................");
-
-		 System.out.println("Amount of stake is : " + INITIAL_STAKE );
-		 System.out.println("Amount to Bet after every game : " + STAKE_BET);
-		 winOrLose();
-		 System.out.println("\nAvailable Amount of Stake :" + INITIAL_STAKE );
-
-	}
-
-}
-
+	
 	
