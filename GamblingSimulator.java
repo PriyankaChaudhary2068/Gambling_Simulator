025253d97e0cdfec7cad64045391ef88e8645cf5
chutki12 <priyankachaudhary2068>
2022-08-08 22:41:32 +0530

    ADDED: This Code For To Gambler Starts Game With Stake And Bet

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -2,11 +2,19 @@ package com.bridgelabz.gamblingSimulator;
 
 
 public class GamblingSimulator {
-
+	
+	static int INITIAL_STAKE = 100;
+    static int STAKE_BET = 1;
+	
 	public static void main(String[] args) {
 		
 		 System.out.println(" Welcome To The Gambling Simulation Problem");
+		 System.out.println("...................");
 
+		 System.out.println("Amount of stake is : " + INITIAL_STAKE );
+		 System.out.println("Amount to Bet after every game : " + STAKE_BET);
 	}
 
 }
+
+	
